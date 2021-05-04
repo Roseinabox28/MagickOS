76ff9b140004cd29145aa7a81f82d6ac2fb03ed5
Rose <48871187+Roseinabox28@users.noreply.github.com>
2021-05-03 17:35:28 -0700

    Added 64 bit

---
+++ src/impl/x86_64/boot/main64.asm
@@ -0,0 +1,16 @@
+global long_mode_start
+
+section .text
+bits 64
+long_mode_start:
+    ; load null into data segment registers
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov sgs, ax
+
+    ; print 'OK'
+    mov dword [0xb8000], 0x2f4b2f4f
+    hlt
