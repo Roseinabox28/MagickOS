d687aebaee45dfa7a3641395aba90148a1b8b549
Rose <48871187+Roseinabox28@users.noreply.github.com>
2021-05-04 22:34:12 -0700

    added keyboard interface

--- src/impl/kernal/main.c
+++ src/impl/kernal/main.c
@@ -1,6 +1,15 @@
+#include "print.h"
+
+
 void kernel_main()
 {
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Hello World");
+    print_str("Welcome to Magick-OS\n");
+    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
+    print_str("\nUser: ");
+    test_input();
+
+
+
 }
